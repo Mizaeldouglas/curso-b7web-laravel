38bba1fcd39bc3417d77429d1e12d5c32ab27b71
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-06-06 13:32:38 -0300

    Aula sobre blade finalizada

--- hello-world-laravel/app/Http/Controllers/SiteController.php
+++ hello-world-laravel/app/Http/Controllers/SiteController.php
@@ -50,21 +50,21 @@ class SiteController extends Controller
     {
         $pessoas = [
             [
-                'image' => 'https://i.pravatar.cc/150?img='.rand(0, 50),
+                'image' => 'https://i.pravatar.cc/150?u=w?img='.rand(0, 50),
                 'nome' => 'Alessandro',
                 'birth' => '29/01/90',
                 'idade' => 29,
             ],
 
             [
-                'image' => 'https://i.pravatar.cc/150?img='.rand(0, 50),
+                'image' => 'https://i.pravatar.cc/150?u=w?img='.rand(0, 50),
                 'nome' => 'Bonieky',
                 'birth' => '10/11/1933',
                 'idade' => 90,
             ],
 
             [
-                'image' => 'https://i.pravatar.cc/150?img='.rand(0, 50),
+                'image' => 'https://i.pravatar.cc/150?u=w?img='.rand(0, 50),
                 'nome' => 'Maria',
                 'birth' => '01/01/2020',
                 'idade' => 2,
